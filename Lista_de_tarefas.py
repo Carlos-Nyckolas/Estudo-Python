5087dc0c410cdfdf2b1593d1faeaa37fdac6859c
Ikazuchi <143460349+Carlos-Nyckolas@users.noreply.github.com>
2023-10-10 15:59:38 -0300

    Update

---
+++ Lista_de_tarefas.py
@@ -0,0 +1,30 @@
+import time
+
+time.sleep(1.5)
+print("0%   █▒▒▒▒▒▒▒▒▒")
+time.sleep(1.5)
+print("10%  ███▒▒▒▒▒▒▒")
+time.sleep(1.5)
+print("30%  █████▒▒▒▒▒")
+time.sleep(1.5)
+print("50%  ███████▒▒▒")
+time.sleep(1.5)
+print("100% ██████████")
+time.sleep(1.5)
+print("")
+print("█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█")
+time.sleep(0.5)
+print("█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█")
+time.sleep(0.5)
+print("█░░║║║╠─║─║─║║║║║╠─░░█")
+time.sleep(0.5)
+print("█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█")
+time.sleep(0.5)
+print("█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█")
+
+
+
+
+
+
+
